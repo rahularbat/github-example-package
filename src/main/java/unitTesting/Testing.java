af43618d385a6549b93c78c575a3fa92da61e9a6
Rahul Arbat <rahul.arbat@equinox.com>
2023-07-09 20:16:42 +0530

    first commit

---
+++ src/main/java/unitTesting/Testing.java
@@ -0,0 +1,16 @@
+package unitTesting;
+
+
+/**
+ *
+ */
+public class Testing {
+
+    public static void display(String param){
+        System.out.println("You parameter is " + param);
+    }
+
+    public void someFunc(){
+        System.out.println("Testing.someFunc");
+    }
+}
